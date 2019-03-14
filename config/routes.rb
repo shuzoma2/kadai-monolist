689b8edf4ce2ac45e09a86cb422d63f7f296642b
shigeo nagashi <shigechikak10@gmail.com>
2019-03-14 10:02:04 +0000

    ranking

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,6 @@ Rails.application.routes.draw do
     resources :items, only: [:show, :new]
     resources :ownerships, only: [:create, :destroy]
     
+    get 'rankings/want', to: 'rankings#want'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
